30412deb0dc27de38b75d678d8d4b5bace4af615
Anjaneya Sharma <23103301@MAIL.JIIT.AC.IN>
2025-05-05 21:13:50 +0530

    configure

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enable static exports
+  distDir: 'dist', // Change build output directory
+  images: {
+    unoptimized: true, // Required for static export
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
+  basePath: '/CodePlag', // Add this if deploying to GitHub Pages under a sub-path
 }
 
 export default nextConfig
